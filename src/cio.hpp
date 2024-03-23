761d3a28d604193a662946bb92f0960e035d10ff
darealshinji <djcj@gmx.de>
2024-03-23 13:30:10 +0100

    Update license headers

--- src/cio.hpp
+++ src/cio.hpp
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (C) 2024 djcj@gmx.de
+ * Copyright (c) 2024 Carsten Janssen
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
