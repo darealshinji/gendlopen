2e2b1e628b7ce9f930402dea12251f01d4db3c5d
darealshinji <djcj@gmx.de>
2024-04-17 01:19:35 +0200

    use std::regex to find SKIP_BEGIN / SKIP_END lines

--- src/parse.cpp
+++ src/parse.cpp
@@ -28,17 +28,19 @@
 
 #include <iostream>
 #include <list>
-#include <string>
-#include <stdlib.h>
-
+#include <regex>
+#include <cstdlib>
 #include "gendlopen.hpp"
+
+#define SKIP_BEGIN 0
+#define SKIP_END 1
 
 
 namespace /* anonymous */
 {
     using list_t = std::list<const char *>;
 
-    /* check for keyword in list */
+    /* check for keywords from a list */
     bool find_keyword(const std::string &line, const list_t &list)
     {
         if (line.find("%%") == std::string::npos) {
@@ -52,6 +54,55 @@ namespace /* anonymous */
         }
 
         return false;
+    }
+
+    /* check for a "%SKIP_BEGIN%" or "%SKIP_END%" line and
+     * return either SKIP_BEGIN, SKIP_END or -1 if nothing was found */
+    int check_skip_keyword(const std::string &line)
+    {
+        const std::regex reg("^\\s*?%SKIP_(BEGIN|END)%\\s*?$");
+        std::smatch m;
+
+        if (!std::regex_match(line, m, reg) || m.size() != 2) {
+            return -1;
+        }
+
+        if (m[1] == "BEGIN") {
+            return SKIP_BEGIN;
+        } else if (m[1] == "END") {
+            return SKIP_END;
+        }
+        return -1;
+    }
+
+    /* loop and replace function prototypes, append to buffer */
+    void replace_function_prototypes(vproto_t &vec, const std::string &line, std::string &buffer)
+    {
+        std::string copy;
+
+        for (const auto &e : vec) {
+            copy = line;
+
+            /* don't "return" on "void" functions */
+            if (utils::eq_str_case(e.type, "void")) {
+                /* keep the indentation pretty */
+                utils::replace("%%return%% ", "", copy);
+                utils::replace("%%return%%", "", copy);
+            } else {
+                utils::replace("%%return%%", "return", copy);
+            }
+
+            if (e.type.ends_with("*")) {
+                /* append space */
+                utils::replace("%%type%% ", e.type, copy);
+            }
+            utils::replace("%%type%%", e.type, copy);
+            utils::replace("%%symbol%%", e.symbol, copy);
+            utils::replace("%%args%%", e.args, copy);
+            utils::replace("%%notype_args%%", e.notype_args, copy);
+
+            buffer += copy;
+        }
     }
 }
 
@@ -88,43 +139,50 @@ std::string gendlopen::parse(std::string &data)
         custom_prefix = true;
     }
 
-    /* change line endings from \r\n to \n */
+    /* Change all line endings from \r\n to \n.
+     * This is a radical solution but it makes parsing the code
+     * a lot easier. */
     utils::replace("\r\n", "\n", data);
 
     /* read data character by character */
     for (const char *p = data.c_str(); *p != 0; p++)
     {
-        /* will be NUL if EOL was reached */
+        /* will be NUL if EOL is reached */
         const char next = *(p+1);
 
-        /* treat lines ending on '@' like single lines */
+        /* treat lines ending on '@' like single lines
+         * but keep the '\n' character */
         if (*p == '@' && next == '\n') {
             p++;
 
             if (comment_out) {
-                /* keep trailing @ symbol in the comments */
+                /* keep trailing @ symbol in commented out section */
                 line += '@';
             } else {
-                /* append newline and continue reading */
                 line += '\n';
                 continue;
             }
         }
 
-        /* add character to line */
         line += *p;
 
-        /* end of line not yet reached */
+        /* end of line or data not yet reached */
         if (*p != '\n' && next != 0) {
             continue;
         }
 
-        /* whether to comment out parts of code */
-        if (line == "%SKIP_BEGIN%\n") {
+        /* check if we have to comment out lines
+         * between "%SKIP_BEGIN%" and "%SKIP_END%" */
+
+        int skip = check_skip_keyword(line);
+
+        if (skip == SKIP_BEGIN) {
+            /* set as "true" if "--skip-parameter-names" was given,
+             * otherwise set as false and nothing is commented out */
             comment_out = m_skip_parameter_names;
             line.clear();
             continue;
-        } else if (line == "%SKIP_END%\n") {
+        } else if (skip == SKIP_END) {
             comment_out = false;
             line.clear();
             continue;
@@ -153,29 +211,7 @@ std::string gendlopen::parse(std::string &data)
                 continue;
             }
 
-            for (const auto &e : m_prototypes) {
-                std::string copy = line;
-
-                /* don't "return" on "void" functions */
-                if (utils::eq_str_case(e.type, "void")) {
-                    /* keep the indentation pretty */
-                    utils::replace("%%return%% ", "", copy);
-                    utils::replace("%%return%%", "", copy);
-                } else {
-                    utils::replace("%%return%%", "return", copy);
-                }
-
-                if (e.type.ends_with("*")) {
-                    /* append space */
-                    utils::replace("%%type%% ", e.type, copy);
-                }
-                utils::replace("%%type%%", e.type, copy);
-                utils::replace("%%symbol%%", e.symbol, copy);
-                utils::replace("%%args%%", e.args, copy);
-                utils::replace("%%notype_args%%", e.notype_args, copy);
-
-                buf += copy;
-            }
+            replace_function_prototypes(m_prototypes, line, buf);
         } else if (has_obj) {
             /* object prototypes */
 
@@ -198,7 +234,7 @@ std::string gendlopen::parse(std::string &data)
         line.clear();
     }
 
-    /* replace the prefixes */
+    /* replace prefixes */
     if (custom_prefix) {
         utils::replace("GDO", m_name_upper, buf);
         utils::replace("gdo", m_name_lower, buf);
