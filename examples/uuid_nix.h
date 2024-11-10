619d7c9202cce7f69145fb8ab7df6a5cd733031f
darealshinji <djcj@gmx.de>
2024-11-10 17:59:07 +0100

    add Clang AST dumps to test w/o the presence of clang

--- examples/uuid_nix.h
+++
@@ -1,4 +0,0 @@
-// library: libuuid.so.1
-// symbols: uuid_generate_random uuid_is_null uuid_unparse_lower
-
-#include <uuid.h>
--- examples/uuid_win.h
+++
@@ -1,8 +0,0 @@
-// library: rpcrt4.dll
-// symbols: UuidCreate UuidToStringA RpcStringFreeA
-
-#ifndef _WIN32
-#error not compiling for win32 platform
-#endif
-
-#include <rpc.h>
