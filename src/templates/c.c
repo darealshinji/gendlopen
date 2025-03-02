fdd137372c6bca44e0615c1b96356c656cbe65f4
darealshinji <djcj@gmx.de>
2025-03-02 13:09:04 +0100

    rewrite gdo_strlcpy and gdo_snprintf based on man page descriptions and examples

--- src/templates/c.c
+++ src/templates/c.c
@@ -69,82 +69,34 @@ GDO_INLINE char *gdo_dladdr_get_fname(const void *ptr);
 /*                                save error                                 */
 /*****************************************************************************/
 
-/* simplified implementation of snprintf (based on NetBSD's version) */
-/*-
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Chris Torek.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-GDO_INLINE void gdo_snprintf(gdo_char_t *str, size_t buflen, const gdo_char_t *fmt, ...)
+/* like snprintf(3), no return value */
+GDO_INLINE void gdo_snprintf(gdo_char_t *str, size_t size, const gdo_char_t *fmt, ...)
 {
     va_list ap;
+
     va_start(ap, fmt);
-
-#ifdef GDO_WINAPI
-    _vsntprintf_s(str, buflen, _TRUNCATE, fmt, ap);
+#ifdef _MSC_VER
+    _vsntprintf_s(str, size, _TRUNCATE, fmt, ap);
 #else
-    vsnprintf(str, buflen, fmt, ap);
+    vsnprintf(str, size, fmt, ap);
 #endif
-
     va_end(ap);
-
-    /* not needed but you never know */
-    str[buflen-1] = 0;
 }
 
-/* simplified implementation of strlcpy (based on NetBSD's version) */
-/*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND TODD C. MILLER DISCLAIMS ALL
- * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL TODD C. MILLER BE LIABLE
- * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-GDO_INLINE void gdo_strlcpy(gdo_char_t *dst, const gdo_char_t *src, size_t dst_len)
+/* like strlcpy(3), no return value */
+GDO_INLINE void gdo_strlcpy(gdo_char_t *dst, const gdo_char_t *src, size_t size)
 {
-    gdo_char_t *dst_end = dst + (dst_len - 1);
+    gdo_char_t *end = dst + size - 1;
 
-    while (dst != dst_end) {
-        if ((*dst++ = *src++) == 0) {
+    for ( ; dst != end; dst++, src++) {
+        *dst = *src;
+
+        if (*src == 0) {
             return;
         }
     }
 
-    *dst_end = 0;
+    *end = 0;
 }
 
 /* save message to error buffer */
