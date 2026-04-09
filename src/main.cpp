501817904244c31f911ef7324afc7bdc1aa3953d
darealshinji <djcj@gmx.de>
2026-04-10 00:39:59 +0200

    don't use SetConsoleCP()

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,10 @@
 int main(int argc, char **argv)
 {
 #if defined(_WIN32) && defined(ENABLE_CP_UTF8)
-    SetConsoleCP(CP_UTF8);
+    /* Set the process code page to UTF-8 with the activeCodePage property in
+     * the appxmanifest during linking and set the console output codepage to
+     * UTF-8 here. SetConsoleCP() is not required and it would keep the new
+     * codepage after the program closes. */
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
